2bb8f62a2a2badb3cbb63324d87f287398a0ea73
LeoOcampoTapias <lmoisture@gmail.com>
2023-11-25 18:22:13 -0500

    creacion de aplicacion react y primeros pasos en el header

---
+++ fasttrackprojectv1/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "./Head";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        <Head />
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
